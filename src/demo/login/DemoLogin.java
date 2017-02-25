20014687cccbf728944a5799fad8d9a2b72f2431
CarienKymdell <carien@itfirst.co.za>
2017-02-25 17:54:15 +0200

    Initial

---
+++ src/demo/login/DemoLogin.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package demo.login;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author i7
+ */
+public class DemoLogin {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        new LoginScreen ().setVisible(true);
+        
+//        String username = null;
+//        username = JOptionPane.showInputDialog("Please enter a username");
+//
+//        if (username.equals("Admin")) {
+//            String password = null;
+//            password = JOptionPane.showInputDialog("Please enter password");
+//            if (password.equals("Password")) {
+//                JOptionPane.showMessageDialog(null, "welcome");
+//
+//            } else {
+//                JOptionPane.showMessageDialog(null, "Password Incorrect");
+//
+//            }
+//        } else {
+//            JOptionPane.showMessageDialog(null, "Username Incorrect");
+//        }
+        // TODO code application logic here
+    }
+
+    
+
+    
+        
+    }
+
